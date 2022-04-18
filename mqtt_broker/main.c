241bdaede68216508ebbd5ace0e74191b10afcd7
Sunrise <cjhh319@163.com>
2022-04-18 21:55:03 +0800

    first porting success

---
+++ mqtt_broker/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include <linux/types.h>
+#include <linux/videodev2.h>
+
+int main()
+{
+	printf("hello world\n");
+	return -1;
+}
